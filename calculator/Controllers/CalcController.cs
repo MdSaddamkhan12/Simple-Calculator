06618562ad602d87c1168ccdd75c70b797a22209
Md Saddam Khan <saddam8101.sk@gmail.com>
2020-04-19 13:57:46 +0530

    My first commit

---
+++ calculator/Controllers/CalcController.cs
@@ -0,0 +1,43 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+        [HttpPost]
+        public ActionResult Index(calc c,string calculate)
+        {
+           if(calculate == "add")
+            {
+                c.Total = c.number_1 + c.number_2;
+
+            }
+            else if (calculate == "sub")
+            {
+                c.Total = c.number_1 - c.number_2;
+
+            }
+            else if (calculate == "mul")
+            {
+                c.Total = c.number_1 * c.number_2;
+
+            }
+            else 
+            {
+                c.Total = c.number_1 / c.number_2;
+
+            }
+            return View(c);
+        }
+    }
+}
---
+++ calculator/Models/calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class calc
+    {
+        public int number_1 { get; set; }
+        public int number_2 { get; set; }
+        public int Total { get; set; }
+    }
+}
